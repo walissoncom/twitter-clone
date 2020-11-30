30f69b349de8933263e993a622f2170d1d3a8dff
Walisson Rodrigues <contact@walisson.com>
2020-11-30 19:30:48 +1100

    Feed component retrieving data (Posts) from Firebase

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import storage from '../../firebase';
 
 import TweetBox from '../TweetBox/TweetBox';
 import Post from '../Post/Post';
@@ -6,26 +8,47 @@ import Post from '../Post/Post';
 import './Feed.css';
 
 function Feed() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+
+        // Firebase storage
+        storage.child('posts').on('value', snapshot => {
+            if (snapshot.val() != null) {
+                setPosts({
+                    ...snapshot.val()
+                })
+            }
+        })
+
+    }, []);
+
     return (
         <div className="feed">
 
-            {/* Header */}
+            {/* Header */}
             <div className="feed__header">
                 <h2>Home</h2>
             </div>
 
-            {/* TweetBox */}
+            {/* TweetBox */}
             <TweetBox />
 
-            {/* Post */}
-            <Post
-                avatar="https://pbs.twimg.com/profile_images/923341083777441792/u5u0GCOs_x96.jpg"
-                displayName="Walisson Rodrigues"
-                username="WalissonOR"
-                verified={false}
-                text="Knowing ReactJS feels like"
-                image="https://media4.giphy.com/media/MC6eSuC3yypCU/giphy.gif" />
-
+            {/* Post */}
+            {
+                Object.keys(posts).map(id => {
+                    return <div key={id}>
+                        <Post
+                            avatar={posts[id].avatar}
+                            displayName={posts[id].displayName}
+                            username={posts[id].username}
+                            verified={posts[id].verified}
+                            text={posts[id].text}
+                            image={posts[id].image} />
+                    </div>
+                })
+            }
         </div>
     )
 }
